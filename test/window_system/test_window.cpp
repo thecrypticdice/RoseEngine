8955e76fde0c15d88e0af8b0c46dc923af0d35f5
Hilal-Almoqbali <72453298+Hilal-Almoqbali@users.noreply.github.com>
2025-07-20 15:16:13 +0400

    update

--- test/window_system/test_window.cpp
+++ test/window_system/test_window.cpp
@@ -2,13 +2,15 @@
 #include<window-sys.h>
 #include<stdlib.h>
 #include<string>
-const char *vertexShaderSource ="#version 330 core\n"
+const char *vertexShaderSource =
+    "#version 330 core\n"
     "layout (location = 0) in vec3 aPos;\n"
     "void main()\n"
     "{\n"
     "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
     "}\0";
-const char *fragmentShaderSource ="#version 330 core\n"
+const char *fragmentShaderSource =
+    "#version 330 core\n"
     "out vec4 FragColor;\n"
     "void main()\n"
     "{\n"
